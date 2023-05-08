228ec3a4d6b8ed0f165ff1054b3b4d25d6d87273
WeslleySORDev <weslleysordev@gmail.com>
2023-05-08 16:14:16 -0300

    create header

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import rocketflixLogo from './assets/rocketflix-logo.svg'
+
+function App() {
+  return (
+    <>
+    <header className='flex flex-col items-center gap-2'>
+      <img className='h-12 w-20' src={rocketflixLogo} alt="Rocketflix Logo" />
+      <h1 className='text-2xl text-center text-[#FFFCF9] font-bold'>Não sabe o que assistir?</h1>
+    </header>
+      <main>
+        <h1>WESLLEY PODEROSO</h1>
+      </main>
+    </>
+  );
+}
+
+export default App;
